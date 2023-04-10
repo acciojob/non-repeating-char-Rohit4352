704e28b24775f3bcb1773eccccf004d7ebfec2a2
Rohit Gupta <65344659+Rohit4352@users.noreply.github.com>
2023-04-10 18:56:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function myFunction(str) {
+  let str = prompt("Enter a string");
+	let res;
+  if (str != null) {
+    document.getElementById("demo").innerHTML = str;
+	res = str;  
+  }
+	alert(res);
+}
